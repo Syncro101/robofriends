507bb0010ee92dace48d833bc63ad9ed87162a25
Syncro101 <syncro10101@gmail.com>
2020-10-20 21:08:23 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A simple front-end react app.
---
+++ precache-manifest.c8eb6c0387ed34600001b59fe9188a59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38edad8696b35540d0f7b02e009ca38a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "208826b182233f236c99",
+    "url": "/robofriends/static/css/main.76db21ec.chunk.css"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends/static/js/2.3788da92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends/static/js/2.3788da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208826b182233f236c99",
+    "url": "/robofriends/static/js/main.896ac4d4.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "6bd7958cb6d24c568a1475af9dfa5a75",
+    "url": "/robofriends/static/media/FuturisticShoreRegular-2JP8.6bd7958c.woff"
+  }
+]);
